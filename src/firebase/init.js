5f43ca9cfb5d09a7ccd12374bed6913778b80836
Saujanya0910 <saujanya.pandey2000@gmail.com>
2021-03-22 00:07:44 +0530

    firebase changes

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,14 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 // Your web app's Firebase configuration
+// use your own api-authentication details here.
 var firebaseConfig = {
-    apiKey: "AIzaSyDoyYwr_KRxRHLLaA8kZLvpRB9r3QxhKBA",
-    authDomain: "ninja-chat-saujanya0910.firebaseapp.com",
-    projectId: "ninja-chat-saujanya0910",
-    storageBucket: "ninja-chat-saujanya0910.appspot.com",
-    messagingSenderId: "856942842963",
-    appId: "1:856942842963:web:5e2a2af54362a6da1da2a6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
